de2b5195492f363a002abeb97ca1f76030746ed1
futabooo <takahiro.futagawa@gmail.com>
2024-02-19 14:50:50 +0900

    modify

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/futabooo/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/futabooo/keymap.c
@@ -24,30 +24,30 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for futabooo (VIA)
   [0] = LAYOUT_universal(
-    KC_ESC        , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_DEL   ,
-    CTL_T(KC_TAB) , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
-    KC_LSFT       , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , XXXXXXX  ,
+    KC_ESC        , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                         KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_DEL   ,
+    CTL_T(KC_TAB) , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                         KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
+    KC_LSFT       , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                         KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT  ,
               KC_LALT , KC_LGUI ,    LT(2,KC_LNG2) , LT(1,KC_SPC) , LT(3,KC_LNG1) ,                   SFT_T(KC_BSPC)  , RCMD_T(KC_ENT) ,       XXXXXXX  , XXXXXXX  , G(A(KC_SPC))
   ),
 
   [1] = LAYOUT_universal(
-    XXXXXXX          , KC_1     , KC_2     , KC_3       , KC_4     , KC_5     ,                                   KC_6     , KC_7     , KC_8        , KC_9        , KC_0       , XXXXXXX     ,
-    CTL_T(KC_GRV)    , KC_EXLM  , KC_AT    , KC_HASH    , KC_DLR   , KC_PERC  ,                                   KC_CIRC  , KC_AMPR  , KC_ASTR     , KC_MINS     , KC_EQL     , KC_BSLS     ,
-    SFT_T(S(KC_GRV)) , XXXXXXX  , XXXXXXX  , S(KC_LBRC) , KC_LBRC  , KC_LPRN  ,                                   KC_RPRN  , KC_RBRC  , S(KC_RBRC)  , S(KC_MINS)  , S(KC_EQL)  , S(KC_BSLS)  ,
+    XXXXXXX          , KC_1     , KC_2     , KC_3     , KC_4        , KC_5     ,                                   KC_6     , KC_7        , KC_8     , KC_9        , KC_0       , XXXXXXX            ,
+    CTL_T(KC_GRV)    , KC_EXLM  , KC_AT    , KC_HASH  , KC_DLR      , KC_PERC  ,                                   KC_CIRC  , KC_AMPR     , KC_ASTR  , KC_MINS     , KC_EQL     , KC_BSLS            ,
+    S(KC_GRV) , XXXXXXX  , XXXXXXX  , KC_LBRC  , S(KC_LBRC)  , KC_LPRN  ,                                   KC_RPRN  , S(KC_RBRC)  , KC_RBRC  , S(KC_MINS)  , S(KC_EQL)  , SFT_T(S(KC_BSLS))  ,
                   _______  , _______  ,           XXXXXXX  ,  XXXXXXX  , XXXXXXX  ,                   _______  , KC_RCMD  ,        XXXXXXX  , XXXXXXX  , _______
   ),
 
   [2] = LAYOUT_universal(
-    XXXXXXX , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                                         XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
-    KC_LCTL , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                                         XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
-    KC_LSFT , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                                         XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
+    XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                                         XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
+    KC_LCTL  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                                         XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
+    KC_LSFT  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                                         XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
                   _______  , _______  ,           XXXXXXX  ,  XXXXXXX  , XXXXXXX  ,                   _______  , _______  ,        XXXXXXX  , XXXXXXX  , _______
   ),
 
   [3] = LAYOUT_universal(
-    XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX   , XXXXXXX  ,                                        XXXXXXX  , XXXXXXX  , KC_UP    , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
-    _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , CPI_I100  , SCRL_DVI ,                                        XXXXXXX  , KC_LEFT  , KC_DOWN  , KC_RGHT  , XXXXXXX  , XXXXXXX  ,
-    _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , CPI_D100  , SCRL_DVD ,                                        XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , KBC_SAVE ,
+    XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX   , XXXXXXX  ,                                       XXXXXXX  , XXXXXXX  , KC_UP    , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
+    _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , CPI_I100  , SCRL_DVI ,                                       XXXXXXX  , KC_LEFT  , KC_DOWN  , KC_RGHT  , XXXXXXX  , XXXXXXX  ,
+    _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , CPI_D100  , SCRL_DVD ,                                       XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , KBC_SAVE ,
                   _______  , _______  ,           QK_BOOT  ,  KBC_RST  , XXXXXXX  ,                   _______  , _______  ,        XXXXXXX  , XXXXXXX  , _______
   ),
 
